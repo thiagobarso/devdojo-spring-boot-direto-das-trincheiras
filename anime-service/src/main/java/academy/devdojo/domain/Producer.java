49c66aee0e69e19374f7e9c3a6f6ff16f04313fb
Thiago Barbosa Soares <thiagobarso@gmail.com>
2025-03-04 00:05:11 -0300

    27) Padrão MVC 02 – Service

--- anime-service/src/main/java/academy/devdojo/domain/Producer.java
+++ anime-service/src/main/java/academy/devdojo/domain/Producer.java
@@ -17,17 +17,6 @@ public class Producer {
     @JsonProperty("name")
     private String name;
     private LocalDateTime createdAt;
-    private static List<Producer> producers = new ArrayList<>();
 
-    static {
-        var mappa = Producer.builder().id(1L).name("Mappa").createdAt(LocalDateTime.now()).build();
-        var kyotoAnimation = Producer.builder().id(2L).name("Kyoto Animation").createdAt(LocalDateTime.now()).build();
-        var madhouse = Producer.builder().id(3L).name("MadHouse").createdAt(LocalDateTime.now()).build();
-        producers.addAll(List.of(mappa, kyotoAnimation, madhouse));
-    }
-
-    public static List<Producer> getProducers() {
-        return producers;
-    }
 }
 
